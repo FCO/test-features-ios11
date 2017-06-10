3cca5612fce31e284b78e98d0943fc6cc9e28d5f
Fernando Correa <fernando.correa@schibsted.com.br>
2017-06-09 22:45:15 -0700

    Initial Commit

--- testCameraIOS/ViewController.swift
+++ testCameraIOS/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
+import AVFoundation
 class ViewController: UIViewController {
-
+    let captureSession = AVCaptureSession()
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var btn: UIButton!
+    var devices: [AVCaptureDevice]?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    @IBAction func toggle(_ sender: UIButton) {
+        if !sender.isSelected {
+            sender.isSelected = true
+            captureSession.sessionPreset = AVCaptureSession.Preset.high
+            self.beginSession(devices![0])
+            lbl.text = "??? 🤔 ???"
+        } else {
+            sender.isSelected = false
+            self.stopSession(devices![0])
+            lbl.text = ""
+        }
+        self.toFront()
+    }
+    func toFront() {
+        for view in [btn as UIView, lbl as UIView] {
+            self.view.bringSubview(toFront: view)
+        }
+    }
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let err : NSError? = nil
+        btn.setTitle("start", for: .normal)
+        btn.setTitle("stop", for: .selected)
+        devices = AVCaptureDevice.devices()
+        do {
+            try captureSession.addInput(AVCaptureDeviceInput(device: devices![0]))
+        } catch {
+            print("error: \(String(describing: err?.localizedDescription) )")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func beginSession(_ captureDevice: AVCaptureDevice) {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.frame = self.view.bounds
+        captureSession.startRunning()
+        self.view.layer.addSublayer(previewLayer!)
     }
-
-
+    
+    func stopSession(_ captureDevice: AVCaptureDevice) {
+        previewLayer?.removeFromSuperlayer()
+        captureSession.stopRunning()
+    }
 }
 
